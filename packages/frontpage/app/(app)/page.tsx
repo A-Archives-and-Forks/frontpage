91f44daa04a1da982e614a7bbda176b3c9b400dd
Tom Sherman <tom@sherman.is>
2026-04-03 13:50:04 +0100

    Refactor feed registry to store AtUris instead of strings (#349)
    * Refactor feed registry to store AtUris instead of strings
    
    * Use strings in the server action layer (AtUris can't be sent between server and client)

--- packages/frontpage/app/(app)/page.tsx
+++ packages/frontpage/app/(app)/page.tsx
@@ -7,13 +7,16 @@ import { HOT_FEED_URI } from "@/lib/feed-constants";
 export default async function Home() {
   await connection();
 
-  const getMorePostsAction = getMoreFeedPostsAction.bind(null, HOT_FEED_URI);
+  const getMorePostsAction = getMoreFeedPostsAction.bind(
+    null,
+    HOT_FEED_URI.toString(),
+  );
   const initialData = await getMorePostsAction(null);
 
   return (
     <FeedSwitcherLayout>
       <InfiniteList
-        cacheKey={`feed:${HOT_FEED_URI}`}
+        cacheKey={`feed:${HOT_FEED_URI.toString()}`}
         getMoreItemsAction={getMorePostsAction}
         emptyMessage="No posts remaining"
         fallback={initialData}
